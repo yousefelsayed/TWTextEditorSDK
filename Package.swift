cd0a0cf7a73ea8512b5a8da67d72a8c43254f218
Yousef Elsayed <yousefe.elsayed@gmail.com>
2024-04-30 19:27:45 +0300

    Add Fonts folder as resources

--- Package.swift
+++ Package.swift
@@ -18,7 +18,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TWTextEditorSDK"),
+            name: "TWTextEditorSDK",
+            dependencies: [],
+            resources: [.process("Fonts")]),
         .testTarget(
             name: "TWTextEditorSDKTests",
             dependencies: ["TWTextEditorSDK"]),
